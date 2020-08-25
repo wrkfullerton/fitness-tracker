b13ed88e833f8fb6778d675b8f5925100d0b91f5
William Fullerton <wrk.fullerton@gmail.com>
2020-08-25 16:00:33 -0500

    changes made to the mongoose file, connected it to Mongo Atlas

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,5 +1,8 @@
-const mongoose = require('mongoose');
-
-const databaseURL = 'workout';
-
-mongoose.connect(process.env.MONGODB_URI || `mongodb://{MONGO_URI}/${databaseURL}`, { useNewUrlParser: true });
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://WRKFr0gs95:WRKFr0gs95@cluster0.vzi3n.mongodb.net/workout?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
