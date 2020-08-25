f31a3e275cd893946ec7f4543dbfebf24d88b66b
William Fullerton <wrk.fullerton@gmail.com>
2020-08-25 16:40:11 -0500

    changes made to the db/ data for mongoose connectivity, and also added a repo README

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,8 +1,5 @@
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://WRKFr0gs95:WRKFr0gs95@cluster0.vzi3n.mongodb.net/workout?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+const mongoose = require("mongoose");
+
+const databaseUrl = 'workout';
+
+mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/${databaseUrl}`, { useNewUrlParser: true });
